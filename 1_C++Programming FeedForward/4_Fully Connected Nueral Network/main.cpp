97e6387c3d4a324074c959aa32f6d91578ae965f
tada20001 <tada20001@gmail.com>
2019-07-14 17:24:45 +0900

    An example of FNN

--- 1_C++Programming FeedForward/4_Fully Connected Nueral Network/main.cpp
+++ 1_C++Programming FeedForward/4_Fully Connected Nueral Network/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "NeuralNetwork.h"
 
 int main()
 {
+	VectorND<D> x(2);
+	x[0] = 0.0; x[1] = 0.0;
+
+	VectorND<D> y_target(2);
+	y_target[0] = 0.3f;
+
+	VectorND<D> y_temp(2);
+
+	NeuralNetwork nn_;
+	nn_.initialize(2, 1, 1);
+	nn_.alpha_ = 0.1;
+
+	for (int i = 0; i < 100; i++)
+	{
+		nn_.setInputVector(x);
+		nn_.propForward();
+
+		nn_.copyOutputVector(y_temp);
+		std::cout << y_temp << std::endl;
+
+		nn_.propBackward(y_target);
+	}
 
 	return 0;
 }
