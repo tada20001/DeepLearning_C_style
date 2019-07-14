bf5dcbdc2c05bdde7c9b4fac5343c0c853ede3cb
tada20001 <tada20001@gmail.com>
2019-07-14 18:18:19 +0900

    Try XOR problem

--- 1_C++Programming FeedForward/4_Fully Connected Nueral Network/main.cpp
+++ 1_C++Programming FeedForward/4_Fully Connected Nueral Network/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include "NeuralNetwork.h"
 
+// XOR-problem
+// if x1 = 0, x2 = 0, y=0
+// if x1 = 0, x2 = 1, y = 1
+// if x1 = 1, x2 = 0, y = 1
+// if x1 = 1, x2 = 1, y = 0
+
+class Example
+{
+public:
+	VectorND<D> x_, y_;
+};
+
+
 int main()
 {
-	VectorND<D> x(2);
-	x[0] = 0.0; x[1] = 0.0;
+	Example xor_problem[4];   // 4 case
 
-	VectorND<D> y_target(1);
-	y_target[0] = 0.3f;
+	// initialize x, y
+	for (int e = 0; e < 4; e++)
+	{
+		xor_problem[e].x_.initialize(2);
+		xor_problem[e].y_.initialize(1);
+	}
+
+	xor_problem[0].x_[0] = 0.0; xor_problem[0].x_[1] = 0.0; xor_problem[0].y_[0] = 0.0;
+	xor_problem[1].x_[0] = 0.0; xor_problem[1].x_[1] = 1.0; xor_problem[1].y_[0] = 1.0;
+    xor_problem[2].x_[0] = 1.0; xor_problem[2].x_[1] = 0.0; xor_problem[2].y_[0] = 1.0;
+	xor_problem[3].x_[0] = 1.0; xor_problem[3].x_[1] = 1.0; xor_problem[3].y_[0] = 0.0;
+
+	//VectorND<D> x(2);
+	//x[0] = 0.0; x[1] = 0.0;
+
+	//VectorND<D> y_target(1);
+	//y_target[0] = 0.3f;
 
 	VectorND<D> y_temp(1); // allocate the memory of output for forward propagation
 
@@ -17,13 +44,18 @@ int main()
 
 	for (int i = 0; i < 1000; i++)
 	{
-		nn_.setInputVector(x);
-		nn_.propForward();
+		for (int e = 0; e < 4; e++)
+		{
+			nn_.setInputVector(xor_problem[e].x_);
+			nn_.propForward();
 
-		nn_.copyOutputVector(y_temp);
-		std::cout << y_temp << std::endl;
+			nn_.copyOutputVector(y_temp);
+			std::cout << xor_problem[e].y_ << " " <<  y_temp << std::endl;
 
-		nn_.propBackward(y_target); // training
+			nn_.propBackward(xor_problem[e].y_); // training
+		}
+		std::cout << std::endl;
+		
 	}
 
 	return 0;
